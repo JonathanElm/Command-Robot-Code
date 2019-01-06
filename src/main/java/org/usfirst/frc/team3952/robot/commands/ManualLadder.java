ab54725c7c88cda5ffbafefef6136b4bb2415044
DoggySazHi <william.le.huy@gmail.com>
2019-01-05 21:32:05 -0800

    Redesigned to be more suitable for VSCode (2019 kickoff)

---
+++ src/main/java/org/usfirst/frc/team3952/robot/commands/ManualLadder.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team3952.robot.commands;
+
+import org.usfirst.frc.team3952.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ManualLadder extends Command {
+    public ManualLadder() {
+        requires(Robot.ladder);
+        setInterruptible(true);
+    }
+
+    protected void initialize() {}
+    
+    protected void execute() {
+    	if(Robot.controller.extendLadder()) {
+			Robot.ladder.extend();
+		} else if(Robot.controller.retractLadder()) {
+			Robot.ladder.retract();
+		} else {
+			Robot.ladder.stop();
+		}
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    	Robot.ladder.stop();
+    }
+    
+    protected void interrupted() {
+    	Robot.ladder.stop();
+    }
+}
